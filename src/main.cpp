cee3ee79f9d378958a562eaaef5672222dbdb2e2
Eiki KITAJIMA <k1010048@edu.cc.uec.ac.jp>
2018-05-31 23:11:39 +0900

    f

---
+++ src/main.cpp
@@ -0,0 +1,80 @@
+/**
+ @file read9axis.ino
+ @brief This is an Example for the FaBo 9Axis I2C Brick.
+
+   http://fabo.io/202.html
+
+   Released under APACHE LICENSE, VERSION 2.0
+
+   http://www.apache.org/licenses/
+
+ @author FaBo<info@fabo.io>
+*/
+
+#include <iostream>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+#include "FaBo9Axis_MPU9250.h"
+
+FaBo9Axis fabo_9axis;
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	if (wiringPiSetup() != 0)
+	{
+		Debug::print(LOG_SUMMARY, "Failed to setup wiringPi!\r\n");
+		return -1;
+	}
+
+	if (fabo_9axis.begin())
+	{
+		Serial.println("configured FaBo 9Axis I2C Brick");
+	}
+	else
+	{
+		Serial.println("device error");
+		while (1);
+	}
+
+	while (true)
+	{
+		float ax, ay, az;
+		float gx, gy, gz;
+		float mx, my, mz;
+		float temp;
+
+		fabo_9axis.readAccelXYZ(&ax, &ay, &az);
+		fabo_9axis.readGyroXYZ(&gx, &gy, &gz);
+		fabo_9axis.readMagnetXYZ(&mx, &my, &mz);
+		fabo_9axis.readTemperature(&temp);
+
+		Serial.print("ax: ");
+		Serial.print(ax);
+		Serial.print(" ay: ");
+		Serial.print(ay);
+		Serial.print(" az: ");
+		Serial.println(az);
+
+		Serial.print("gx: ");
+		Serial.print(gx);
+		Serial.print(" gy: ");
+		Serial.print(gy);
+		Serial.print(" gz: ");
+		Serial.println(gz);
+
+		Serial.print("mx: ");
+		Serial.print(mx);
+		Serial.print(" my: ");
+		Serial.print(my);
+		Serial.print(" mz: ");
+		Serial.println(mz);
+
+		Serial.print("temp: ");
+		Serial.println(temp);
+
+		delay(1000);
+	}
+}
+
